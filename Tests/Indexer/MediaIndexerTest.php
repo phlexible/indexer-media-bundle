17e405e9ca3eeb0ba9df089396456b7709e7d5a2
Stephan Wentz <swentz@brainbits.net>
2016-10-20 12:50:35 +0200

    fix docblocks, add LICENSE

--- Tests/Indexer/MediaIndexerTest.php
+++ Tests/Indexer/MediaIndexerTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the phlexible indexer media package.
+ *
+ * (c) Stephan Wentz <sw@brainbits.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Phlexible\Bundle\IndexerMediaBundle\Tests\Indexer;
 
 use Phlexible\Bundle\IndexerBundle\Document\DocumentIdentity;
