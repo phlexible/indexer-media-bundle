837d9a30817c1066b553d98585785912dc3f0dae
Stephan Wentz <swentz@brainbits.net>
2017-03-08 10:37:59 +0100

    fix commands

--- Command/AddCommand.php
+++ Command/AddCommand.php
@@ -50,7 +50,7 @@ class AddCommand extends ContainerAwareCommand
         $indexer = $this->getContainer()->get('phlexible_indexer_media.media_indexer');
         $storage = $indexer->getStorage();
 
-        $output->writeln('Indexer: '.$indexer->getName());
+        $output->writeln('Indexer: '.get_class($indexer));
         $output->writeln('  Storage: '.get_class($storage));
         $output->writeln('    DSN: '.$storage->getConnectionString());
 
